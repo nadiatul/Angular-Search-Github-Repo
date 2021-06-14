c8c2aa201fcb2fe4703fe7b5f5a768964e78f3ee
Nadia <nadiatul.alawiyah@gmail.com>
2021-06-14 11:27:23 +0800

    Remove Unnecessary Codes
    Remove Unnecessary Codes for Unit Test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -19,32 +19,11 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  // it(`should have as title 'angular-search-github-api'`, () => {
-  //   const fixture = TestBed.createComponent(AppComponent);
-  //   const app = fixture.componentInstance;
-  //   expect(app.title).toEqual('angular-search-github-api');
-  // });
-
-  // it('should render title', () => {
-  //   const fixture = TestBed.createComponent(AppComponent);
-  //   fixture.detectChanges();
-  //   const compiled = fixture.nativeElement;
-  //   expect(compiled.querySelector('.content span').textContent).toContain('angular-search-github-api app is running!');
-  // });
 
   it('should filter the specific keywords in the list', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-
-    const input: any = { target: { value: 'grit' }};
-    // const result = app.applyFilter(input);
     const result = app.testingFilter('grit');
-
-    // const newResult = result.map((e: any) => { return e = Object.values(e).join(' ')})
-    // const extraNewResult = newResult.join('');
-    console.log(' result' , result);
-    // console.log(' newResult' , newResult);
-    // console.log(' extraNewResult' , extraNewResult);
     expect(result).toBe(true);
   });
 });
